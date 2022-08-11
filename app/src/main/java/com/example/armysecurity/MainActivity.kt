b56f8a579b25c35a7db01b30c5ece4bba0799a87
dlwpdlf147 <dlwpdlf147@naver.com>
2022-08-11 23:26:55 +0900

    toolbar setting

--- app/src/main/java/com/example/armysecurity/MainActivity.kt
+++ app/src/main/java/com/example/armysecurity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.armysecurity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -10,19 +11,27 @@ import com.example.armysecurity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
+    private lateinit var navController:NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initNavBottom()
+        initNavigation()
     }
-    private fun initNavBottom(){
-        binding.navBottom.setupWithNavController(
-            (supportFragmentManager.findFragmentById(R.id.mainFragment) as NavHostFragment)
-                .navController
-        )
+    private fun initNavigation(){
+        navController = (supportFragmentManager.findFragmentById(R.id.mainFragment)
+                as NavHostFragment).navController
 
+        binding.navBottom.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener{_, destination, _ ->
+            when(destination.id){
+                R.id.tab_btm_myPage -> binding.mainToolbarName.text = getString(R.string.tab_btm_myPage)
+                R.id.tab_btm_event -> binding.mainToolbarName.text = getString(R.string.tab_btm_event)
+                R.id.tab_btm_search -> binding.mainToolbarName.text = getString(R.string.tab_btm_search)
+            }
+        }
     }
 }
