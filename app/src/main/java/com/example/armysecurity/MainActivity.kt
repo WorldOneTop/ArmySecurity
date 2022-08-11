291c5a9182e8813c35a98e2ea2f66b2701115b49
dlwpdlf147 <dlwpdlf147@naver.com>
2022-08-11 23:10:32 +0900

    navigation setting

--- app/src/main/java/com/example/armysecurity/MainActivity.kt
+++ app/src/main/java/com/example/armysecurity/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.armysecurity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.armysecurity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initNavBottom()
+    }
+    private fun initNavBottom(){
+        binding.navBottom.setupWithNavController(
+            (supportFragmentManager.findFragmentById(R.id.mainFragment) as NavHostFragment)
+                .navController
+        )
+
     }
 }
